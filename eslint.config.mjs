0bf9c5fa5005a4fa7865a1175c060d9714ec0c30
david-balko <davidbalko10@gmail.com>
2025-09-19 11:52:46 +0300

    Testing my pipeline

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,23 @@
-import globals from "globals";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import pluginJest from "eslint-plugin-jest";
 
 export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+	{ files: ["**/*.js"], languageOptions: { globals: globals.node } },
+	{ files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ['**/*.spec.js', '**/*.test.js'],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      'jest/no-disabled-tests': 'warn',
+      'jest/no-focused-tests': 'error',
+      'jest/no-identical-title': 'error',
+      'jest/prefer-to-have-length': 'warn',
+      'jest/valid-expect': 'error',
+    },
+  },
 ]);
